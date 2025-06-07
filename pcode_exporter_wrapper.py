edbaebfe014ecefeceaf1b25b390d3e738fb7f69
nhagan2468 <natalie.hagan@trojans.dsu.edu>
2025-06-07 17:53:07 -0400

    adding license information and tweaking README

--- pcode_exporter_wrapper.py
+++ pcode_exporter_wrapper.py
@@ -3,8 +3,23 @@
 #@nhagan2468 
 #@category PCode 
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 # EDIT THIS!!! Change this to the appropriate location for the output
-outFile = "\\Path\\to\\output\\file"
+outFile = "C:\\Users\\Natalie\\Documents\\foo\\foou"
 
 # Set the location as the argument and then run the export_pcode script
 setScriptArgs([outFile])
